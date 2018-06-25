ae8c15e9590d27c6e4d0d19807b4033cc75eaa1a
Michael <Michael@Michael-Pc>
2018-06-25 12:14:29 +0200

    Aufsteigend

--- src/bll/Shakersort.java
+++ src/bll/Shakersort.java
@@ -15,19 +15,20 @@ public class Shakersort extends SortingMethod {
 		for (int i = 0; i < arrayToSort.size() / 2; i++) {
 			boolean swapped = false;
 			for (int j = i; j < arrayToSort.size() - i - 1; j++) {
-				if (arrayToSort.get(j) < arrayToSort.get(j + 1)) {
+				if (arrayToSort.get(j) > arrayToSort.get(j + 1)) {
 					switchElements(arrayToSort, j, j + 1);
 					swapped = true;
 				}
 			}
 			for (int j = arrayToSort.size() - 2 - i; j > i; j--) {
-				if (arrayToSort.get(j) > arrayToSort.get(j - 1)) {
+				if (arrayToSort.get(j) < arrayToSort.get(j - 1)) {
 					switchElements(arrayToSort, j, j - 1);
 					swapped = true;
 				}
 			}
 			if (!swapped)
 				break;
+			f.drawArray(arrayToSort);
 		}
 
 	}
